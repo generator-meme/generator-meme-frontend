f75e1783200786a63863df93fd841f4becd8c20c
lesjok <lesjok@inbox.ru>
2023-01-22 15:48:56 +0300

    add and remove text input

--- src/components/EditorMeme/EditorMeme.jsx
+++ src/components/EditorMeme/EditorMeme.jsx
@@ -2,8 +2,13 @@ import './EditorMeme.css'
 import back from '../../images/back.svg'
 import { Link } from 'react-router-dom'
 import meme from '../../images/meme-edit.jpg'
+import { useState } from 'react'
 
 const EditorMeme = () => {
+
+  const [textTop, setTextTop] = useState(false);
+  const [textBottom, setTextBottom] = useState(false);
+
   return (
     <div className="editor">
       <Link to="/generator-meme-frontend/" className="editor__link">
@@ -12,18 +17,20 @@ const EditorMeme = () => {
       <h2 className="editor__title">Редактор мемов</h2>
       <section className="editor__hero">
         <div className="editor__column_left">
-          <input className="editor__input editor__input_top" type="text" />
-          <input className="editor__input editor__input_bottom" type="text" />
+          {textTop && <input className="editor__input editor__input_top" type="text" />}
+          {textBottom && <input className="editor__input editor__input_bottom" type="text" />}      
           <img src={meme} alt="Мем" className="editor__image" />
         </div>
         <div className="editor__column_right">
-          <button className="editor__button editor__button_top" type="button">
+          <button className="editor__button editor__button_top"
+            type="button"
+            onClick={() => setTextTop(!textTop)}>
             Текст сверху
           </button>
           <button
             className="editor__button editor__button_bottom"
             type="button"
-          >
+            onClick={() => setTextBottom(!textBottom)}>
             Текст снизу
           </button>
         </div>
